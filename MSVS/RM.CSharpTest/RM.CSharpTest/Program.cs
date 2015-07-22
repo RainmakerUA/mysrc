f0a67d16b199d484540204106229fc82e39f04b4
Vasiliy Zhushma <vasiliy.zhusma@matrix42.com>
2015-07-22 16:21:13 +0300

    C# 6 feature test

---
+++ MSVS/RM.CSharpTest/RM.CSharpTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace RM.CSharpTest
+{
+	internal class Program
+    {
+        private static void Main(string[] args)
+        {
+			var tester = new Tester("Mr. Tester", "mrtester@example.com");
+			tester.TestingCompleted += (sender, message) => { Console.WriteLine(message); };
+
+			tester.AddTests(Enumerable.Range(0, 100).Select(i => DateTime.Today.AddDays(i).AddHours(9).ToString("s")));
+			tester.Start();
+
+			Thread.Sleep(29000);
+
+			tester.Stop();
+			Console.WriteLine($"{tester.Name} tested {tester.TestedCount} so far");
+
+			Console.WriteLine("Testing goes on. Press ENTER when your want");
+			tester.Start();
+
+			Console.ReadLine();
+			tester.Stop();
+        }
+    }
+}
