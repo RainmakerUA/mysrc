2a8f912ce076e6be37aac74503ea7017ee339957
VasiliyZhushma <vasiliy.zhusma@matrix42.com>
2017-06-21 14:10:43 +0300

    [*] Improve ExchangeAttachment name and data retrieval

--- MSVS/Matrix42.Client.Mail/Matrix42.Client.Mail/Exchange/Attachment.cs
+++ MSVS/Matrix42.Client.Mail/Matrix42.Client.Mail/Exchange/Attachment.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Matrix42.Client.Mail.Utility;
 using Microsoft.Exchange.WebServices.Data;
 
 namespace Matrix42.Client.Mail.Exchange
@@ -26,34 +28,64 @@ namespace Matrix42.Client.Mail.Exchange
 
 		public static Attachment From(Microsoft.Exchange.WebServices.Data.Attachment attachment)
 		{
-			using (var ms = new MemoryStream())
-			{
-				byte[] data;
-
-				switch (attachment)
-				{
-					case FileAttachment fAtt:
-						fAtt.Load(ms);
-						data = ms.ToArray();
-						break;
-
-					case ItemAttachment iAtt:
-						iAtt.Load(ItemSchema.MimeContent);
-						data = iAtt.Item.MimeContent.Content;
-						break;
-
-					default:
-						data = Encoding.ASCII.GetBytes($"Attachment type {attachment.GetType().FullName} not supported!");
-						break;
-				}
-
-				return new Attachment(attachment.Name, attachment.ContentId, attachment.ContentType, data);
-			}
+			return new Attachment(GetName(attachment), attachment.ContentId, attachment.ContentType, GetData(attachment));
 		}
 
 		public static IList<IAttachment> ListFrom(AttachmentCollection attachments)
 		{
 			return attachments?.Select(From).ToArray<IAttachment>();
+		}
+
+		private static string GetName(Microsoft.Exchange.WebServices.Data.Attachment attachment)
+		{
+			switch (attachment)
+			{
+				case FileAttachment fAtt:
+					return FileHelper.MakeValidFileName(fAtt.FileName ?? fAtt.Name, fAtt.ContentType);
+
+				case ItemAttachment iAtt:
+					iAtt.Load(ItemSchema.Subject);
+					/*new PropertySet(BasePropertySet.FirstClassProperties, ItemSchema.TextBody, ItemSchema.Attachments, ItemSchema.MimeContent*/
+
+					string subject = null;
+					iAtt.Load(ItemSchema.Subject));
+
+					try
+					{
+						subject = iAtt.Item.Subject;
+					}
+					catch (ServiceObjectPropertyException e)
+					{
+						Console.WriteLine(e); // TODO: Log?
+					}
+
+					return FileHelper.MakeValidFileName(subject, null) + FileHelper.EmlExtension;
+
+				default:
+					// return null;
+					// return FileHelper.MakeValidFileName(null, part.ContentType.MimeType);
+					throw new NotSupportedException($"{attachment.GetType().FullName} is not supported");
+			}
+		}
+
+		private static byte[] GetData(Microsoft.Exchange.WebServices.Data.Attachment attachment)
+		{
+			switch (attachment)
+			{
+				case FileAttachment fAtt:
+					using (var ms = new MemoryStream())
+					{
+						fAtt.Load(ms);
+						return ms.ToArray();
+					}
+
+				case ItemAttachment iAtt:
+					iAtt.Load(ItemSchema.MimeContent);
+					return iAtt.Item.MimeContent.Content;
+
+				default:
+					return Encoding.ASCII.GetBytes($"Attachment type {attachment.GetType().FullName} not supported!");
+			}
 		}
 	}
 }
