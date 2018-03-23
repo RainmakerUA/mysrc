5515be5b1705d8bd12a82d0e4a851632ea984f9e
VasiliyZhushma <vasiliy.zhusma@matrix42.com>
2018-03-23 14:33:28 +0200

    [*] Update dependencies [*] Hardcore Off365 Exchange settings (M42+AK) [+] Add SMTP SendMail test

--- MSVS/Matrix42.Client.Mail/Matrix42.Client.Mail.Console/Program.cs
+++ MSVS/Matrix42.Client.Mail/Matrix42.Client.Mail.Console/Program.cs
@@ -1,4 +1,8 @@
-﻿using Con = System.Console;
+﻿using System.IO;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+using Con = System.Console;
 
 namespace Matrix42.Client.Mail.Console
 {
@@ -6,11 +10,11 @@ namespace Matrix42.Client.Mail.Console
 	{
 		private static void Main(string[] args)
 		{
-			var parserTest = new ParserTest();
-			parserTest.Execute();
+			//var parserTest = new ParserTest();
+			//parserTest.Execute();
 
-			//var imapTest = new ExchangeClientTest();
-			//imapTest.Execute();
+			var imapTest = new ExchangeClientTest();
+			imapTest.Execute();
 
 			//var mimeTest = new MimeTest();
 			//mimeTest.Execute();
@@ -18,8 +22,32 @@ namespace Matrix42.Client.Mail.Console
 			//var msgParserTest = new ParseMsgTest();
 			//msgParserTest.Execute();
 
+			//Con.Write("Gimme HTML file: ");
+
+			//var path = Con.ReadLine()?.Trim();
+			//SendTest(path);
+
 			Con.Write("Press any key...");
 			Con.ReadKey(true);
+		}
+
+		private static void SendTest(string htmlPath)
+		{
+			var msg = new MimeMessage
+						{
+							Subject = "Bold spaces are weirdo!",
+							Body = new TextPart(TextFormat.Html){ Text = File.ReadAllText(htmlPath) }
+						};
+			msg.From.Add(new MailboxAddress("Update4u Mail", "update4u.info@gmail.com"));
+			msg.To.Add(new MailboxAddress("AK = You", "andreykostenko@matrix42Office365GER.onmicrosoft.de"));
+
+			using (var client = new SmtpClient())
+			{
+				client.Connect("smtp.gmail.com", 587);
+				client.Authenticate("update4u.info@gmail.com", "update4u.");
+				client.Send(msg);
+				client.Disconnect(true);
+			}
 		}
 	}
 }
