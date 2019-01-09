5bad7c1fb1e8b6b4f3b88de7784c7cfc9cdc17e7
Vasyl Zhushma <vasiliy.zhusma@matrix42.com>
2019-01-09 13:29:55 +0200

    [-] M42 Mail client: migrated to own repo

--- MSVS/Matrix42.Client.Mail/Matrix42.Client.Mail/Exchange/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Matrix42.Client.Mail.Exchange
-{
-	internal static class Constants
-	{
-		public static readonly string UndeliverableMessageClass = "REPORT.IPM.Note.NDR";
-		public static readonly string OutOfOfficeMessageClass = "IPM.Note.Rules.OofTemplate.Microsoft";
-	}
-}
--- MSVS/Matrix42.Client.Mail/Matrix42.Client.Mail/Utility/IWriteable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Matrix42.Client.Mail.Utility
-{
-	internal interface IWriteable
-	{
-		void WriteTo(Stream stream);
-	}
-}
