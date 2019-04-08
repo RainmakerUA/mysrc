41148ba0c77f1a17a707976b2facca713fb7d54c
Vasyl Zhushma <vasiliy.zhusma@matrix42.com>
2019-04-08 14:25:36 +0300

    [+] Error-tolerant gzip (de)compressor

---
+++ MSVS/RM.Util.GZip/RM.Util.GZip/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace RM.Util.GZip
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			if (args.Length < 3)
+			{
+				ShowUsageAndExit();
+			}
+
+			var isUnpackMode = false;
+
+			switch (args[0].ToLowerInvariant())
+			{
+				case "u":
+				case "unpack":
+					isUnpackMode = true;
+					break;
+
+				case "p":
+				case "pack":
+					isUnpackMode = false;
+					break;
+
+				default:
+					ShowUsageAndExit();
+					break;
+			}
+
+			try
+			{
+				if (isUnpackMode)
+				{
+					Unpack(args[1], args[2]);
+				}
+				else
+				{
+					Pack(args[1], args[2]);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Error: {e.GetType().Name} :: {e.Message}");
+			}
+		}
+
+		private static void Pack(string inFile, string outFile)
+		{
+			using (var ifs = File.OpenRead(inFile))
+			{
+				using (var ofs = File.OpenWrite(outFile))
+				{
+					using (var gzs = new GZipStream(ofs, CompressionMode.Compress, true))
+					{
+						ifs.CopyTo(gzs);
+					}
+				}
+			}
+		}
+
+		private static void Unpack(string inFile, string outFile)
+		{
+			using (var ifs = File.OpenRead(inFile))
+			{
+				using (var ofs = File.OpenWrite(outFile))
+				{
+					using (var gzs = new GZipStream(ifs, CompressionMode.Decompress, true))
+					{
+						gzs.CopyTo(ofs);
+					}
+				}
+			}
+		}
+
+		private static void ShowUsageAndExit()
+		{
+			Console.WriteLine($"Usage: {Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().Location)} p(ack)|u(npack) in-file out-file");
+			Environment.Exit(-19);
+		}
+	}
+}
