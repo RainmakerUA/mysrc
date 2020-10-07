1c4ee59a6a7e0e5a30c3ee294bd7ae818273abc4
Vasyl Zhushma <vasiliy.zhusma@matrix42.com>
2020-10-07 11:36:44 +0300

    Add projects for implementing WPF localization

---
+++ MSVS/RM.Lib.Wpf/RM.Lib.Wpf.Common/MarkupExtensions/Helper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace RM.Lib.Wpf.Common.MarkupExtensions
+{
+	public static class Helper
+	{
+		public static T GetService<T>(this IServiceProvider provider) where T : class => provider.GetService(typeof(T)) as T;
+	}
+}
